0bf191d0e2c964f8bb2c0c505481986fbd6bff49
danielzinhors <danielzinhors@gmail.com>
2023-10-29 16:50:08 -0300

    Add porta do server mais swagger ajustes no env

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/danielzinhors/multithreading-go-expert/configs"
+	"github.com/danielzinhors/multithreading-go-expert/infra/webservers/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
+// @title           Consulta CEP Go Expert API multithreading
+// @version         1.0
+// @description     Prodct API Consulta CEP
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Daniel Silveira
+// @contact.url    http://danielsilveira.dev.br
+// @contact.email  falecom@danielsilveira.dev.br
+
+// @license.name   Daniel Silveira
+// @license.url    http://danielsilveira.dev.br
+
+// @host   localhost:8000
+// @basePath  /
 func main() {
 	configs, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
-	EndPointViaCep := configs.WebServerViaCep
-	if EndPointViaCep == "" {
-		panic("Endpoint Via CEP não encontrado na config")
-	}
-	EndPointApiCep := configs.WebserviceApiCep
-	if EndPointApiCep == "" {
-		panic("Endpoint Api CEP não encontrado na config")
-	}
 
+	cepHandler := handlers.NewCepHanbler()
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Route("/cep", func(r chi.Router) {
+		r.Get("/", cepHandler.BuscaCep)
+	})
+	portServer := ":" + configs.WebServerPort
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost"+portServer+"/docs/doc.json")))
+	http.ListenAndServe(portServer, r)
 }
